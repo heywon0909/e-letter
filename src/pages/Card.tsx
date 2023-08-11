3da19565df7c1d4a2ba3ec5e80fe3dd806db3808
heywon0909 <hw7319@naver.com>
2023-08-12 00:12:08 +0900

    card 디자인 수정

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -7,7 +7,7 @@ export default function Card() {
     const { cardId,cardImgNum } = useParams();
     return (
          <section className='w-full h-full flex flex-col justify-center items-center'>
-            <div className='lg:w-2/5 md:w-3/5 w-11/12 h-full flex flex-col justify-center items-center -mt-16'>
+            <div className='lg:w-2/5 md:w-3/5 w-11/12 h-full flex flex-col justify-center items-center'>
                 {cardId && <CardTemplate cardId={cardId} cardImgNum={cardImgNum as string} />}
             </div>    
          </section>
--- src/pages/CardSelect.tsx
+++ src/pages/CardSelect.tsx
@@ -26,7 +26,7 @@ export default function CardSelect() {
     },[])
 
     return (
-       <section className='w-full h-auto relative md:top-0 top-24'>  
+       <section className='w-full md:h-full h-auto relative md:top-0 top-24'>  
             <div className='w-full h-full flex flex-col justify-center items-center'>
                 <h2 className='font-mono mt-2 text-base'>편지지 양식을 골라주세요.</h2>
         <div className='md:w-3/4 w-11/12 flex justify-center space-y-3'>
