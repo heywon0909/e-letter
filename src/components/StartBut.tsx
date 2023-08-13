2b41f2af4e0e5b692ff74285f765712f360e8875
heywon0909 <hw7319@naver.com>
2023-08-13 19:43:30 +0900

    환경변수로 원복

--- src/components/StartBut.tsx
+++ src/components/StartBut.tsx
@@ -12,7 +12,7 @@ export default function StartBut() {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             //@ts-ignore
             Kakao.Auth.authorize({
-                redirectUri: 'https://heywon0909.github.io/e-letter/login/oauth',
+                redirectUri: import.meta.env.VITE_KAKAO_REDIRECT_URI,
                 scope:"profile_nickname,account_email,gender,profile_image"
             });
         }
