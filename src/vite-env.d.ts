d0fd6509bcea67b6cd865802f68f47f8a94712fd
heywon0909 <hw7319@naver.com>
2023-08-13 20:44:49 +0900

    env interface 선언

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,7 +6,13 @@ declare global {
   }
 }
 interface ImportMetaEnv {
-  readonly VITE_CUSTOM_ENV_VARIABLE: string;
+  readonly VITE_KAKAO_JAVASCRIPT_KEY: string;
+  readonly VITE_KAKAO_REST_API_KEY: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
+  readonly VITE_CLOUDINARY_URL: string;
+  readonly VITE_CLOUDINARY_KEY: string;
+  readonly VITE_UPLOAD_PRESET: string;
+  readonly VITE_CRYPTOJS_SECRET_KEY: string;  
 }
 
 interface ImportMeta {
