72bc92dceea3a61b41c590ac5cc8a78ee2f9b0f3
heywon0909 <hw7319@naver.com>
2023-08-13 21:17:15 +0900

    vite config ts env 설정 중..

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,19 @@ export default defineConfig({
       { find: '@img', replacement: path.resolve(__dirname, 'public/img') },
     ],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        intro: `
+          import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY = "${process.env.VITE_KAKAO_JAVASCRIPT_KEY}";
+          import.meta.env.VITE_KAKAO_REST_API_KEY = "${process.env.VITE_KAKAO_REST_API_KEY}";
+          import.meta.env.VITE_KAKAO_REDIRECT_URI = "${process.env.VITE_KAKAO_REDIRECT_URI}";
+          import.meta.env.VITE_CLOUDINARY_URL = "${process.env.VITE_CLOUDINARY_URL}";
+          import.meta.env.VITE_CLOUDINARY_KEY = "${process.env.VITE_CLOUDINARY_KEY}";
+          import.meta.env.VITE_UPLOAD_PRESET = "${process.env.VITE_UPLOAD_PRESET}";
+          import.meta.env.VITE_CRYPTOJS_SECRET_KEY = "${process.env.VITE_CRYPTOJS_SECRET_KEY}";
+        `,
+      },
+    },
+  },
 })
