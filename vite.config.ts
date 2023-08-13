03a25271f15a9c9e2ba40fcd20122c629048339a
heywon0909 <hw7319@naver.com>
2023-08-13 21:46:52 +0900

    vite env 변수 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
 
-// https://vitejs.dev/config/
-const viteEnv = {}
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]
-  }
-})
 
-export default defineConfig({
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({command, mode })=>{
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
   plugins: [react()],
   base: '/e-letter/',
-  define:viteEnv,
+  define: {
+    VITE_KAKAO_JAVASCRIPT_KEY: JSON.stringify(env.VITE_KAKAO_JAVASCRIPT_KEY),
+    VITE_KAKAO_REST_API_KEY: JSON.stringify(env.VITE_KAKAO_REST_API_KEY),
+    VITE_KAKAO_REDIRECT_URI: JSON.stringify(env.VITE_KAKAO_REDIRECT_URI),
+    VITE_CLOUDINARY_URL: JSON.stringify(env.VITE_CLOUDINARY_URL),
+    VITE_CLOUDINARY_KEY: JSON.stringify(env.VITE_CLOUDINARY_KEY),
+    VITE_UPLOAD_PRESET: JSON.stringify(env.VITE_UPLOAD_PRESET),
+    VITE_CRYPTOJS_SECRET_KEY: JSON.stringify(env.VITE_CRYPTOJS_SECRET_KEY),
+  },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
@@ -21,4 +26,5 @@ export default defineConfig({
       { find: '@img', replacement: path.resolve(__dirname, 'public/img') },
     ],
   },
+}
 })
